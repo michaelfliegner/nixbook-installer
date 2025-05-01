f8f15f6b30bff03bb26735c7025105043ef6d330
Michael Kelly <mike@membervault.co>
2025-04-30 20:02:25 -0700

    flake and lock

--- flake.nix
+++ flake.nix
@@ -9,25 +9,20 @@
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          system = system;
-        };
-      in {
-        packages.install-iso = pkgs.nixosConfigurations.installer.config.system.build.isoImage;
-
-        nixosConfigurations.installer = pkgs.lib.nixosSystem {
+        nixosConfiguration = nixpkgs.lib.nixosSystem {
           system = system;
           modules = [
             "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares-gnome.nix"
 
-            # Add any of your customizations here:
             {
               nixpkgs.config.allowUnfree = true;
               isoImage.isoName = "nixos-custom-installer.iso";
-              system.stateVersion = "24.05"; # adjust to your desired version
+              system.stateVersion = "24.05";
             }
           ];
         };
+      in {
+        packages.install-iso = nixosConfiguration.config.system.build.isoImage;
       });
 }
 
